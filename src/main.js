3cbb9ad8eb8fcf7cc43492d1a71373e181fb09e8
LinkinAndy1 <624177925@qq.com>
2020-07-23 16:59:08 +0800

    提交测试

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import echarts from 'echarts'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import store from './store/index.js'
+import request from './assets/js/request.js'
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts
+Vue.prototype.$axios = axios
+Vue.prototype.$store = store
+Vue.prototype.$H = request
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
